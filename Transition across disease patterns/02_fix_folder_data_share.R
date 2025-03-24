6062d8be79471dfcf358383bb23134ab4d8bc442
Caterina Gregorio <caterina.gregorio@outlook.com>
2025-03-24 18:17:53 +0100

    fix datasets

---
+++ Transition across disease patterns/02_fix_folder_data_share.R
@@ -0,0 +1,25 @@
+
+source("Transition across disease patterns/utils/prepare_csv.R")
+
+
+load("Transition across disease patterns/Simulation Outputs/pop_study_2yr2_100_3000.RData")
+prepare_csv(pop2,100,"schema_xa_3000")
+
+load("Transition across disease patterns/Simulation Outputs/pop_study_2_100_3000.RData")
+prepare_csv(pop,100,"schema_xb_3000")
+
+load("Transition across disease patterns/Simulation Outputs/pop_study_iv2_100_3000.RData")
+prepare_csv(pop,100,"schema_xc_3000")
+
+
+
+load("Transition across disease patterns/Simulation Outputs/pop_study_2yr_under2_100_3000.RData")
+prepare_csv(pop2_under,100,"schema_xa_under_3000")
+
+load("Transition across disease patterns/Simulation Outputs/pop_study_under2_100_3000.RData")
+prepare_csv(pop2_under,100,"schema_xb_under_3000")
+
+load("Transition across disease patterns/Simulation Outputs/pop_study_iv_under2_100_3000.RData")
+prepare_csv(pop2_under,100,"schema_xc_under_3000")
+
+
---
+++ Transition across disease patterns/utils/prepare_csv.R
@@ -0,0 +1,24 @@
+
+prepare_csv <- function(input,nsim,name){
+  lapply(1:nsim,function(x)fix_one_dataset(x,input,name))
+}
+
+fix_one_dataset <- function(x,input,name){
+  
+require(tidyverse)
+require(magrittr)
+dir.create(paste0("Transition across disease patterns/Simulated Data/",name))
+
+data_ready <- input %>% filter(dataset_id==x) %>% 
+  rename(subject_id=patient_id,
+         cov1=dm_sex,
+         cov2=educ_el,
+         age_baseline=Age_entry) %>%
+  mutate(age_exit=min(Age_exit,Age_death,na.rm = T),
+         dht=ifelse(!is.na(Age_death),1,0)) %>% 
+  select(-Age_death,-Age_exit,-contains("MP"),-lag_age,-age_group) %>% 
+  select(dataset_id,subject_id,age_baseline,age_exit,time_in_study,dht,cov1,cov2,visit_number,age,ndis,everything())
+print(colnames(data_ready))
+write.csv(data_ready,file = paste0("Transition across disease patterns/Simulated Data/",name,"/",name,"_dataset",x))
+  
+}
